9453ab4ce6a5927684d5339aded041157e91638b
Alexandros Pyrgiotakis <alex.ierap@gmail.com>
2017-12-20 21:15:31 +0200

    Website files

---
+++ scripts.js
@@ -0,0 +1,32 @@
+
+
+$(function main(){
+var typed1 = new Typed('.bx_1',{
+  strings:['Alexandros Pyrgiotakis'],
+  loop:false,
+  typeSpeed:80
+
+});
+});
+
+$(function run_1(){
+  var remove_cursor = new Typed('.bx_3',{
+    strings: ['My name is Alexandros and I am 19 years old.I am from Ierapetra Crete Greece but i currently live in Kastoria.I am a student at the Techological Educational Institute of Western Macedonia,studying software engineering. Currently developing my coding skills  in <span class="lng">PHP</span>,<span class="lng">Javascript</span> and <span class="lng">C++</span> but mostly i code in web-based languages.I also use new technologies like, <span class="tech">React</span>, <span class="tech">node.js</span> for a better user experience. I love what i do and i spend most of my free time reading and practicing to get better at coding.'],
+    loop: false,
+    backDelay:700,
+    typeSpeed:40,
+
+  });
+return true;
+});
+
+  $(function run_1(){
+    var intrsts_func = new Typed('.bx_2',{
+      strings: [,'What am i interested in?', 'I am interested in <span style="color:red;">Coding</span>!', 'I am interested in <span style="color:red;">Computers</span>!', 'I am interested in <span style="color:red;">Computer games</span>!', 'I am interested in <span style="color:red;">Motorcycle riding</span>!'],
+      loop: true,
+      backDelay:700,
+      typeSpeed:70,
+      backSpeed:70
+
+    });
+  });
